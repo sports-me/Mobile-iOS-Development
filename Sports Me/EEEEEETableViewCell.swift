28e8e3a9811b7599ce606176a98fd2789d14bc32
RomanYerevan <khachaturhakobyan2018@gmail.com>
2019-04-17 17:58:07 +0400

    New Commit

---
+++ Sports Me/EEEEEETableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EEEEEETableViewCell.swift
+//  Sports Me
+//
+//  Created by Khachatur Hakobyan on 4/17/19.
+//  Copyright © 2019 Khachatur Hakobyan. All rights reserved.
+//
+
+import UIKit
+
+class EEEEEETableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
