f67869564f0ccdb54f81109f719ea43b47a09205
RomanYerevan <khachaturhakobyan2018@gmail.com>
2019-04-18 13:27:30 +0400

    Lilit Added

---
+++ Sports Me/LilitTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LilitTableViewCell.swift
+//  Sports Me
+//
+//  Created by Khachatur Hakobyan on 4/18/19.
+//  Copyright © 2019 Khachatur Hakobyan. All rights reserved.
+//
+
+import UIKit
+
+class LilitTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
